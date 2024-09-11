c83f0935fff8a753a216bfb2f9ef78c219c7d7dc
dingd <dingdi1981@hotmail.com>
2024-09-11 20:00:47 +0800

    modify expression.lisp

--- static/lisp-code/expression.lisp
+++ static/lisp-code/expression.lisp
@@ -5,3 +5,28 @@
 
 ;; 7 atoms of lisp
 ; car, cdr, cons, eq, atom, cond, lambda
+
+
+; https://windtunnel.cn/posts/001-rude-start-application/
+; https://windtunnel.cn/posts/002-lazy-process/
+; https://windtunnel.cn/posts/003-upsidedown-infix/
+; https://windtunnel.cn/posts/004-perfect.md/
+; https://windtunnel.cn/posts/005-explore-lisp/
+; https://windtunnel.cn/posts/006-sequence-in-lisp/
+; https://windtunnel.cn/posts/007-recursive-eq/
+; https://windtunnel.cn/posts/008-expression/
+
+
+(require 'explore-lisp)
+
+(el:search-symbols "stop" 'hunchentoot)
+
+(el:export-all-external-symbols 'hunchentoot)
+
+
+;; start hunchentoot server with public as root
+(defun start-server ()
+  (start (make-instance 'easy-acceptor :port 8000 :document-root "E:/lisp-writing/qchen-fdii-cardc.github.io/public")))
+
+;; stop hunchentoot server
+(start-server)
