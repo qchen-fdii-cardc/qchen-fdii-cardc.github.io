4c2eeff92674c69ddfbb1fb537b47a4bfb41fc53
dingd <dingdi1981@hotmail.com>
2024-10-13 14:55:06 +0800

    Update matlab 19

--- temp-files/matlab/ballOnPeaks.m
+++ temp-files/matlab/ballOnPeaks.m
@@ -1,8 +1,9 @@
-function ballOnPeaks(visible, fn)
+function ballOnPeaks(n, visible, fn)
 
     arguments
+        n (1, 1) int32 {mustBeNumeric, mustBePositive} = 10
         visible string {validatestring(visible, ["on", "off"])} = "on"
-        fn {mustBeTextScalar} = "sliding.gif"
+        fn {mustBeTextScalar} = "gridVrand.gif"
     end
 
     % Fix gif extension for exportgraphics
@@ -10,42 +11,70 @@ function ballOnPeaks(visible, fn)
         fn = sprintf("%s.gif", fn);
     end
 
-    [sx, sy, sz] = peaks(500);
+    [sx, sy, sz] = peaks(n);
+    pointCount = n * n;
 
-    F = scatteredInterpolant(sx(:), sy(:), sz(:));
-    func = @(x, y)F(x, y) + 0.5;
-
-    nframes = 100;
-
-    h = figure(Visible = visible);
-    % h = figure;
-
-    surf(sx, sy, sz, EdgeColor = "none", FaceAlpha = 0.85);
-    hold on
-    m = plot3(0.0, 0.0, func(0.0, 0.0), "o", ...
-        "MarkerFaceColor", "red", ...
-        "MarkerSize", 14);
-    axis off
+    szMax = max(sz(:));
 
     xmin = min(sx, [], 'all');
     xmax = max(sx, [], 'all');
     ymin = min(sy, [], 'all');
     ymax = max(sy, [], 'all');
 
+    % rand - Random number generator, uniform distribution in [0, 1]
+    randX = @()(rand() * (xmax - xmin) + xmin);
+    randY = @()(rand() * (ymax - ymin) + ymin);
+
+    F = scatteredInterpolant(sx(:), sy(:), sz(:));
+    func = @(x, y)F(x, y);
+
+    h = figure(Visible = visible);
+
+    surf(sx, sy, sz, EdgeColor = "none", FaceAlpha = 0.75);
+    set(gca, xlim = [-3, 3], ylim = [-3, 3], zlim = [-12, 12]);
+    view(-45, 30);
+    axis off
+    grid off
+    box off
+    hold on
+
+    x0 = randX();
+    y0 = randY();
+    z0 = func(x0, y0);
+
+    m = plot3(x0, y0, z0, "o", ...
+        "MarkerFaceColor", "red", ...
+        "MarkerSize", 5);
+
+    mBest = plot3(x0, y0, z0, "v", ...
+        "MarkerFaceColor", "green", ...
+        "MarkerSize", 8);
+
     if exist(fn, 'file')
         delete(fn);
     end
 
-    exportgraphics(h, fn, Resolution = 150);
+    title(sprintf("Walk count %3d/%3d - Highest position: %.2f/%.2f", 1, pointCount, z0, szMax));
 
-    velocity = 1;
-    randPos = @()(rand() - 0.5) * velocity;
-    coerce = @(val, vmin, vmax)min(vmax, max(val, vmin));
+    exportgraphics(gca, fn, Resolution = 100, Append = true);
 
-    for t = 1:nframes
-        m.XData = coerce(m.XData + randPos(), xmin, xmax);
-        m.YData = coerce(m.YData + randPos(), ymin, ymax);
+    for t = 1:pointCount - 1
+        m.XData = randX();
+        m.YData = randY();
         m.ZData = func(m.XData, m.YData);
+
+        if mBest.ZData < m.ZData
+            mBest.XData = m.XData;
+            mBest.YData = m.YData;
+            mBest.ZData = m.ZData;
+        end
+
+        % set(gca, xlim = [-3, 3], ylim = [-3, 3], zlim = [-12, 12]);
+        title(sprintf("Walk count %3d/%3d - Highest position: %.2f/%.2f", t + 1, pointCount, mBest.ZData, szMax));
+        % view(-45, 30);
+
         drawnow
-        exportgraphics(h, fn, Resolution = 150, Append = true);
+        exportgraphics(gca, fn, Resolution = 100, Append = true);
     end
+
+    close(h);
