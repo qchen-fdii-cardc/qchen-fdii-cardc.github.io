0e093557bc969da620b87a0a5c3a111a32b422a5
Q. Chen <qchen2015@hotmail.com>
2024-11-26 17:47:20 +0800

    Update 002, typo in 001

--- static/javascript/coolprop-first.js
+++ static/javascript/coolprop-first.js
@@ -8,38 +8,6 @@ function text2key(text) {
         return 'D';
 }
 
-
-// click calculation
-//using jQuery
-$('#calc').click(function () {
-    console.log("clicked calc");
-    var name = $('#FluidName :selected').text()
-    var key1 = text2key($('#Name1 :selected').text())
-    var key2 = text2key($('#Name2 :selected').text())
-    var val1 = parseFloat($('#Value1').val())
-    var val2 = parseFloat($('#Value2').val())
-
-    var T = Module.PropsSI('T', key1, val1, key2, val2, name)
-    var rho = Module.PropsSI('D', key1, val1, key2, val2, name)
-    var p = Module.PropsSI('P', key1, val1, key2, val2, name)
-    var s = Module.PropsSI('S', key1, val1, key2, val2, name)
-    var h = Module.PropsSI('H', key1, val1, key2, val2, name)
-    var cp = Module.PropsSI('C', key1, val1, key2, val2, name)
-
-    text = ''
-    text += 'T = ' + T + ' K\n' + '<br>'
-    text += 'rho = ' + rho + ' kg/m&#179; <br>'
-    text += 'p = ' + p + ' Pa<br>'
-    text += 's = ' + s + ' J/kg/K<br>'
-    text += 'h = ' + h + ' J/kg<br>'
-    text += 'cp = ' + cp + ' J/kg/K<br>'
-
-    $("#output").html(text);
-
-});
-
-
-
 function flush_fluid_list() {
 
     if (Module.get_global_param_string) {
@@ -58,6 +26,36 @@ function flush_fluid_list() {
 
 
 $(function () {
+    // click calculation
+    //using jQuery
+    $('#calc').click(function () {
+        console.log("clicked calc");
+        var name = $('#FluidName :selected').text()
+        var key1 = text2key($('#Name1 :selected').text())
+        var key2 = text2key($('#Name2 :selected').text())
+        var val1 = parseFloat($('#Value1').val())
+        var val2 = parseFloat($('#Value2').val())
+
+        var T = Module.PropsSI('T', key1, val1, key2, val2, name)
+        var rho = Module.PropsSI('D', key1, val1, key2, val2, name)
+        var p = Module.PropsSI('P', key1, val1, key2, val2, name)
+        var s = Module.PropsSI('S', key1, val1, key2, val2, name)
+        var h = Module.PropsSI('H', key1, val1, key2, val2, name)
+        var cp = Module.PropsSI('C', key1, val1, key2, val2, name)
+
+        text = ''
+        text += 'T = ' + T + ' K\n' + '<br>'
+        text += 'rho = ' + rho + ' kg/m&#179; <br>'
+        text += 'p = ' + p + ' Pa<br>'
+        text += 's = ' + s + ' J/kg/K<br>'
+        text += 'h = ' + h + ' J/kg<br>'
+        text += 'cp = ' + cp + ' J/kg/K<br>'
+
+        $("#output").html(text);
+
+    });
+
+
     setTimeout(function () {
         flush_fluid_list();
         // default values
