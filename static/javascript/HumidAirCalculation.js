1a62302266bc5852e1af1e080c0623a3493ee195
Qin CHEN@p15 <qchen2015@hotmail.com>
2024-11-27 20:18:07 +0800

    Humid Air app

---
+++ static/javascript/HumidAirCalculation.js
@@ -0,0 +1,34 @@
+
+const parameters = {
+    "Wet-Bulb Temperature": 'B',
+    "Dew-Point Temperature": 'D',
+    "Mixture enthalpy per dry air": 'H',
+    "Mixture enthalpy per humid air": "Hha",
+    "Water mole fraction": "Y",
+    "Pressure": "P",
+    "Partial pressure of water vapor": "P_w",
+    "Relative humidity in [0, 1]": 'R',
+    "Mixture entropy per unit dry air": "S",
+    "Mixture entropy per unit humid air": "Sha",
+    "Dry-Bulb Temperature": "T",
+    "Mixture volume per unit dry air": "V",
+    "Mixture volume per unit humid air": "Vha",
+    "Humid ratio": "W"
+};
+
+const parameterUnits = {
+    "Wet-Bulb Temperature": 'K',
+    "Dew-Point Temperature": 'K',
+    "Mixture enthalpy per dry air": 'J/kg',
+    "Mixture enthalpy per humid air": "J/kg",
+    "Water mole fraction": "mol/mol",
+    "Pressure": "Pa",
+    "Partial pressure of water vapor": "Pa",
+    "Relative humidity in [0, 1]": '',
+    "Mixture entropy per unit dry air": "J/kg-K",
+    "Mixture entropy per unit humid air": "J/kg-K",
+    "Dry-Bulb Temperature": "K",
+    "Mixture volume per unit dry air": "m^3/kg",
+    "Mixture volume per unit humid air": "m^3/kg",
+    "Humid ratio": "kg/kg"
+}
