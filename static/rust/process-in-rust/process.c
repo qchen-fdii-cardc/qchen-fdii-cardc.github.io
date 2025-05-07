b9902cc54bbee292adc09a0fb43876233f174a06
Qin CHEN@p15 <qchen2015@hotmail.com>
2025-05-07 09:33:26 +0800

    fully functioned and well-behaved process in c

--- static/rust/process-in-rust/process.c
+++ static/rust/process-in-rust/process.c
@@ -18,10 +18,16 @@ int main()
         printf("child process\n");
         char *args[] = {"ls", "-l", NULL};
         execv("/bin/ls", args);
+        perror("execv /bin/ls failed.");
+        return 127;
     }
     else
     {
         printf("parent process\n");
+        // wait for child process to exit
+        int status;
+        wait(&status);
+        printf("child process exited with status %d\n", status);
     }
     return 0;
 }
